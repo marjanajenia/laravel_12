69483e23d387953569f1fb52b49d5ece3e47bde5
marjanajenia <marjanajenia98@gmail.com>
2025-07-17 18:18:11 +0600

    module works perfectly

---
+++ Modules/Blog/routes/web.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Modules\Blog\Http\Controllers\BlogController;
+
+// Route::middleware(['auth', 'verified'])->group(function () {
+//     Route::resource('blogs', BlogController::class)->names('blog');
+// });
+ Route::get('/blog', [BlogController::class, 'index'])->name('index');
