d2aef553e53c45c8d2c2aba82498f2e3a551d785
marjanajenia <marjanajenia98@gmail.com>
2025-07-31 23:48:47 +0600

    profile image part design add

--- resources/views/backend/layouts/setting/profile_setting.blade.php
+++ resources/views/backend/layouts/setting/profile_setting.blade.php
@@ -1,6 +1,15 @@
 @extends('backend.app')
 
 @section('user-title', 'Profile Setting')
+<style>
+    .custom-tabs .tab-link{
+        color:#6c757d;;
+    }
+    .custom-tabs .tab-link.active {
+    color: #28a745;
+    border-bottom: 2px solid #28a745;
+}
+</style>
 
 @section('backend-content')
     <div class="page-content">
@@ -8,11 +17,36 @@
             <!-- end page title -->
             <div class="row">
                 <div class="p-0 m-auto col-md-11">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="d-flex flex-wrap align-items-center">
+                                <div class="profile rounded" style="width: 125px; height: 125px; overflow: hidden;">
+                                    <img src="{{ asset('default/profile.jpg') }}" alt="profile_img"
+                                    style="width: 100%; height: 100%; object-fit: cover; border-radius: 50%;">
+                                </div>
+                                <div class="ms-4">
+                                    <h4>{{ Auth::user()->name ?? 'N/A' }}</h4>
+                                    <h4>{{ Auth::user()->email ?? 'N/A' }}</h4>
+                                    <a href="#" class="btn btn-success btn-sm" id="uploadImageBtn">
+                                        <i class="fa fa-rss"></i> Update Profile
+                                    </a>
+                                    <input type="file" name="profile_picture"
+                                     id="profile_picture_input" style="display: none;">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="border-top px-4 pt-3" style="font-size: medium">
+                            <ul class="nav custom-tabs"  role="tablist">
+                                <li class="me-3">
+                                    <a href="#profile" class="tab-link active" data-bs-toggle="tab" role="tab">Edit Profile</a>
+                                </li>
+                                <li>
+                                    <a href="#password" class="tab-link" data-bs-toggle="tab" role="tab">Update Password</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
 
-                    <ul class="nav">
-                        <li><a href="#profile" class="active show" data-bs-toggle="tab">Edit Profile</a></li>
-                         <li><a href="#password" data-bs-toggle="tab">Update Password</a></li>
-                    </ul>
 
                     <div class="tab-content">
                     <!-- Profile Setting Tab -->
@@ -21,7 +55,7 @@
                                 {{--  <div class="card-header bg-white mt-2">
                                     <h3>Profile Setting</h3>
                                 </div>  --}}
-                                <div class="card-body bg-light">
+                                <div class="card-body">
                                     <form action="{{ route('setting.profile.update') }}" method="POST">
                                         @csrf
                                         <div class="mb-4 row">
