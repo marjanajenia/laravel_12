3946e08f4fe43f09a36cb76ddb24b591f0415913
marjanajenia <marjanajenia98@gmail.com>
2025-07-31 22:50:39 +0600

    bootstrap tab in profile setting

--- resources/views/backend/layouts/setting/profile_setting.blade.php
+++ resources/views/backend/layouts/setting/profile_setting.blade.php
@@ -8,99 +8,81 @@
             <!-- end page title -->
             <div class="row">
                 <div class="p-0 m-auto col-md-11">
-                    <div class="card">
-                        <div class="mt-2 bg-white card-header">
-                            <h3>Profile Setting</h3>
-                        </div>
-                        <div class="card-body bg-light">
-                            <div class="row">
-                                <div class="p-0 m-auto col-md-10">
+
+                    <ul class="nav">
+                        <li><a href="#profile" class="active show" data-bs-toggle="tab">Edit Profile</a></li>
+                         <li><a href="#password" data-bs-toggle="tab">Update Password</a></li>
+                    </ul>
+
+                    <div class="tab-content">
+                    <!-- Profile Setting Tab -->
+                        <div class="tab-pane fade show active" id="profile">
+                            <div class="card mb-4">
+                                {{--  <div class="card-header bg-white mt-2">
+                                    <h3>Profile Setting</h3>
+                                </div>  --}}
+                                <div class="card-body bg-light">
                                     <form action="{{ route('setting.profile.update') }}" method="POST">
                                         @csrf
                                         <div class="mb-4 row">
-                                            <label for="" class="col-sm-3 col-form-label">Name</label>
+                                            <label class="col-sm-3 col-form-label">Name</label>
                                             <div class="col-sm-9">
-                                                <input type="text" class="form-control" name="name" id="name"
-                                                 value="{{ $user->name ?? 'N/A' }}" placeholder="Enter Your Name">
-                                                @error('name')
-                                                    <span class="text-danger">{{ $message }}</span>
-                                                @enderror
+                                                <input type="text" name="name" class="form-control" value="{{ $user->name ?? '' }}"
+                                                    placeholder="Enter Your Name">
+                                                @error('name') <span class="text-danger">{{ $message }}</span> @enderror
                                             </div>
                                         </div>
                                         <div class="mb-4 row">
-                                            <label for="" class="col-sm-3 col-form-label">Email</label>
+                                            <label class="col-sm-3 col-form-label">Email</label>
                                             <div class="col-sm-9">
-                                                <input type="text" class="form-control" name="email" id="email"
-                                                 value="{{ $user->email ?? 'N/A' }}" placeholder="Enter Youe Email Address">
-                                                @error('email')
-                                                    <span class="text-danger">{{ $message }}</span>
-                                                @enderror
+                                                <input type="email" name="email" class="form-control" value="{{ $user->email ?? '' }}"
+                                                    placeholder="Enter Your Email Address">
+                                                @error('email') <span class="text-danger">{{ $message }}</span> @enderror
                                             </div>
                                         </div>
                                         <div class="row justify-content-end">
                                             <div class="col-sm-9">
-                                                <div>
-                                                    <button type="submit"
-                                                        class="btn btn-success waves-effect waves-light w-md">
-                                                        <i class="align-middle bx bx-save font-size-16 me-2"></i>
-                                                        Update
-                                                    </button>
-                                                </div>
+                                                <button type="submit" class="btn btn-success w-md">
+                                                    <i class="bx bx-save me-2"></i> Update
+                                                </button>
                                             </div>
                                         </div>
                                     </form>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="card">
-                        <div class="mt-2 bg-white card-header">
-                            <h3>Password Setting</h3>
-                        </div>
-                        <div class="card-body bg-light">
-                            <div class="row">
-                                <div class="p-0 m-auto col-md-10">
+                        <!-- Password Setting Tab -->
+                        <div class="tab-pane fade" id="password">
+                            <div class="card">
+                                <div class="card-body bg-light">
                                     <form action="{{ route('setting.profile.updatePassword') }}" method="POST">
                                         @csrf
                                         <div class="mb-4 row">
-                                            <label for="" class="col-sm-3 col-form-label">Current Password</label>
+                                            <label class="col-sm-3 col-form-label">Current Password</label>
                                             <div class="col-sm-9">
-                                                <input type="password" class="form-control" name="old_password" id="old_password"
-                                                 placeholder="Enter Current Password">
-                                                @error('old_password')
-                                                    <span class="text-danger">{{ $message }}</span>
-                                                @enderror
+                                                <input type="password" name="old_password" class="form-control" placeholder="Current Password">
+                                                @error('old_password') <span class="text-danger">{{ $message }}</span> @enderror
                                             </div>
                                         </div>
                                         <div class="mb-4 row">
-                                            <label for="" class="col-sm-3 col-form-label">New Password</label>
+                                            <label class="col-sm-3 col-form-label">New Password</label>
                                             <div class="col-sm-9">
-                                                <input type="password" class="form-control" name="password" id="password"
-                                                  placeholder="Enter New password">
-                                                @error('password')
-                                                    <span class="text-danger">{{ $message }}</span>
-                                                @enderror
+                                                <input type="password" name="password" class="form-control" placeholder="New Password">
+                                                @error('password') <span class="text-danger">{{ $message }}</span> @enderror
                                             </div>
                                         </div>
                                         <div class="mb-4 row">
-                                            <label for="" class="col-sm-3 col-form-label">Confirm Password</label>
+                                            <label class="col-sm-3 col-form-label">Confirm Password</label>
                                             <div class="col-sm-9">
-                                                <input type="password" class="form-control" name="password_confirmation" id="password_confirmation"
-                                                placeholder="Enter New password">
-                                                @error('password_confirmation')
-                                                    <span class="text-danger">{{ $message }}</span>
-                                                @enderror
+                                                <input type="password" name="password_confirmation" class="form-control" placeholder="Confirm Password">
+                                                @error('password_confirmation') <span class="text-danger">{{ $message }}</span> @enderror
                                             </div>
                                         </div>
                                         <div class="row justify-content-end">
                                             <div class="col-sm-9">
-                                                <div>
-                                                    <button type="submit"
-                                                        class="btn btn-success waves-effect waves-light w-md">
-                                                        <i class="align-middle bx bx-save font-size-16 me-2"></i>
-                                                        Update
-                                                    </button>
-                                                </div>
+                                                <button type="submit" class="btn btn-success w-md">
+                                                    <i class="bx bx-save me-2"></i> Update
+                                                </button>
                                             </div>
                                         </div>
                                     </form>
